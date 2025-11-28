47910cba0bc5fd1e47ecd4e5705735cf111d9f0b
iradics <75294569+iradics1974@users.noreply.github.com>
2025-11-28 19:21:07 +0100

    Initial python API commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+import socket
+import platform
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Python API running in Kubernetes!", 200
+
+@app.route("/info")
+def info():
+    return jsonify({
+        "host": socket.gethostname(),
+        "system": platform.system(),
+        "release": platform.release(),
+    })
+
+@app.route("/health")
+def health():
+    return "OK", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
